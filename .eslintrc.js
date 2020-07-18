8447a85aa92b02c4e2a17f9309b57bebf1e57de4
kaimo <kaimo313@foxmail.com>
2020-07-18 17:07:56 +0800

    初始化计数器项目的基本结构

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'space-before-function-paren': 0, // 0 = off, 1 = warn, 2 = error
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
